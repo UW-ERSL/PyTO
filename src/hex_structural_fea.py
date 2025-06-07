c5729b8265f94d44f9cbc8eaadddd84f23cad074
UW-ERSL <ksuresh@wisc.edu>
2025-06-07 14:59:05 -0500

    First version of topopt_stl_recovery and .vtu files

--- src/hex_structural_fea.py
+++ src/hex_structural_fea.py
@@ -649,10 +649,10 @@ class HexStructuralFEA:
 if __name__ == "__main__":    
   from hex_structural_examples import StructuralExamples,getStructuralProblem
 
-  problem = StructuralExamples.TensilePlate
-  nDOFDesired = 15000
+  problem = StructuralExamples.LBracketThick
+  nDOFDesired = 200000
   mesh, mat_prop, bc,elem_body_force = getStructuralProblem(problem,nDOFDesired = nDOFDesired)
-  solver = linear_solvers.Solvers.PARDISO # typically DPCG or PARDISO
+  solver = linear_solvers.Solvers.DPCG # typically DPCG or PARDISO
 
   dsolver = deflation.DeflationSolver()
   startTime = time.time()
--- src/topopt_mma.py
+++ src/topopt_mma.py
@@ -240,7 +240,7 @@ if __name__ == "__main__":
 	print("-" * 50)
  
 	to_problem = StructuralTOExamples.LBracketMidLoadStressObjective # Choose the TO problem
-	to_problem = StructuralTOExamples.LBracketMidLoad # Choose the TO problem
+	to_problem = StructuralTOExamples.LBracketThickTopLoad # Choose the TO problem
 	#to_problem = StructuralTOExamples.Inverter # Choose the TO problem
 
 	if (to_problem in StructuralTOExamples):
